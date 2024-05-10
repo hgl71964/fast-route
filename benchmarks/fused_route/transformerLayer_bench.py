b63b556039570f247dbf718a36e75bde54d751f7
Guoliang He <guolianghe1996@gmail.com>
2024-05-10 10:48:07 +0100

    fix

--- benchmarks/fused_route/transformerLayer_bench.py
+++ benchmarks/fused_route/transformerLayer_bench.py
@@ -45,11 +45,11 @@ def parse_args():
         type=int,
         default=1,
     )
-    parser.add_argument(
-        '--seq',
-        type=int,
-        default=128,
-    )
+    # parser.add_argument(
+    #     '--seq',
+    #     type=int,
+    #     default=4096,
+    # )
     parser.add_argument(
         '--n_head',
         type=int,
@@ -106,8 +106,8 @@ if __name__ == '__main__':
 
     @triton.testing.perf_report(
         triton.testing.Benchmark(
-            x_names=['m'],  # Argument names to use as an x-axis for the plot
-            x_vals=[2**i for i in range(0, 15)
+            x_names=['seq'],  # Argument names to use as an x-axis for the plot
+            x_vals=[2**i for i in range(5, 15)
                     ],  # Different possible values for `x_name`
             line_arg=
             'provider',  # Argument name whose value corresponds to a different line in the plot
@@ -128,9 +128,8 @@ if __name__ == '__main__':
                 'topk': topk,
             },
         ))
-    def benchmark(m, provider, n, k, e, topk):
+    def benchmark(seq, provider, n, k, e, topk):
 
-        m = m
         # n = 14336 // 2
         # k = 4096
         # e = 8
@@ -139,7 +138,7 @@ if __name__ == '__main__':
         # torch.cuda.manual_seed(3227)
         dtype = torch.float16
 
-        x = torch.randn((args.b, args.seq, args.k), device='cuda', dtype=dtype) / 10
+        x = torch.randn((args.b, seq, args.k), device='cuda', dtype=dtype) / 10
         gate = torch.randn((k, e), device='cuda', dtype=dtype) / 10
         w1 = torch.randn((e, 2 * n, k), device='cuda', dtype=dtype) / 10
         w2 = torch.randn((e, k, n), device='cuda', dtype=dtype) / 10
@@ -154,9 +153,9 @@ if __name__ == '__main__':
             ms, min_ms, max_ms = triton.testing.do_bench(
                 lambda: vllm_transformer(attn, x, gate, w1, w2, topk, k),
                 quantiles=quantiles)
-        perf = lambda ms: (2 * m * n * k + 2*args.b*args.seq*args.seq*args.n_head*args.head_dim) * 1e-12 / (ms * 1e-3)
+        perf = lambda ms: (2 * args.b * seq * n * k + 2*args.b*seq*seq*args.n_head*args.head_dim) * 1e-12 / (ms * 1e-3)
         return perf(ms), perf(max_ms), perf(min_ms)
 
     benchmark.run(show_plots=True,
                   print_data=True,
-                  save_path=f'./data/moe_bench_{n}_{k}_{e}_{topk}')
+                  save_path=f'./data/transformer_bench_{n}_{k}_{e}_{topk}')
